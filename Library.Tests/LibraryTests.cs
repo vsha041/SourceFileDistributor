4a4de85bbd5e7ba3e3159de43d4737e93a0f2d54
varunsharma1986 <varun.varunsharma@gmail.com>
2017-02-18 13:07:32 +1300

    Added a test to test positive flow of the application.

---
+++ Library.Tests/LibraryTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Library.Tests
+{
+    [TestClass]
+    public class LibraryTests
+    {
+        [TestMethod]
+        public void CheckIfNumberOfSourceFilesEqualsDistributedFiles()
+        {
+            string sourcePath = @"C:\Projects\Repos\SourceFileDistributor\Library.Tests\TestFiles";
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+                    +
+                    @"\"
+                    +
+                    Guid.NewGuid().ToString("N")
+                    +
+                    @"_"
+                    +
+                    Guid.NewGuid().ToString("N")
+                    + @"\";
+
+            var service = new Service("*.cs", sourcePath, path);
+            var timeInSeconds = service.Execute();
+            Assert.IsTrue(timeInSeconds > 0);
+
+            var directories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories).ToList();
+
+            int total = 0;
+            foreach (var directory in directories)
+            {
+                var files = Directory.GetFiles(directory, "*.cs");
+                files = files.Where(f => !f.Contains("TemporaryGeneratedFile")
+                                                     &&
+                                                     !f.Contains("AssemblyInfo.cs")
+                                                     &&
+                                                     !f.Contains("GlobalSuppressions.cs")).ToArray();
+                total += files.Length;
+            }
+            Assert.AreEqual(total, 6 );
+        }
+    }
+}
